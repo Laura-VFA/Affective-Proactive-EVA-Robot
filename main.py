803c248f61cc752b6c844cec1729b892358a483c
Laura-VFA <laura.villa@uclm.es>
2022-02-26 12:27:14 +0100

    src (old directory) removed

--- main.py
+++
@@ -1,9 +0,0 @@
-from UI import start
-from reminder import set_reminder
-from threading import Thread
-
-if __name__ == '__main__':
-    Thread(target = start).start()
-    Thread(target = set_reminder('hola', 10)).start()
-
-    # hacer join?
