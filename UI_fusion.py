26eff290aa9b189e55fa033a8fa1c3e3427b1a6a
Laura-VFA <laura.villa@uclm.es>
2022-03-23 11:51:40 +0100

    transition process with queue notifications (face recognition not working)

--- UI_fusion.py
+++ UI_fusion.py
@@ -5,6 +5,7 @@ from services.eva_led import *
 from services.wakeface import Wakeface
 from services.mic import Recorder
 from services.speaker import Speaker
+import queue
 
 
 # Logging configuration
@@ -12,76 +13,118 @@ logging.basicConfig(filename='./logs/UI.log', format='%(asctime)s - %(levelname)
 #log = logging.getLogger('STDERR')
 
 
+notifications = queue.Queue()
+
 def wf_event_handler(event, username=None, progress=None):
     global eva_state, _username
-    logging.info(f'WF Event {event}')
-    if event == 'face_listen' and eva_state in ['idle', 'recording_face']:
+    notification = {
+        'transition': '',
+        'params': {
+            'username':username,
+            'progress': progress
+        }
+    }
+    if event == 'face_listen' and eva_state == 'idle':
+        notification['transition'] = 'idle2listening'
+        notifications.put(notification)
+    elif event in ['face_not_listen', 'not_faces'] and eva_state == 'listening':
+        notification['transition'] = 'listening2idle'
+        notifications.put(notification)
+
+        if event == 'not_faces':
+            _username = None
+    elif event == 'recording_face':
+        notification['transition'] = 'recording_face'
+        notifications.put(notification)
+    
+
+def mic_event_handler(event, audio=None):
+    notification = {
+        'transition': '',
+        'params': {
+            'audio': audio
+        }
+    }
+    if event == 'start_recording' and eva_state in ['listening', 'listening_without_cam']:
+        if eva_state == 'listening':
+            notification['transition']  = 'listening2recording'
+        elif eva_state == 'listening_without_cam':
+            notification['transition']  = 'listening_without_cam2recording'
+        notifications.put(notification)
+    if event == 'stop_recording' and eva_state == 'recording':
+        notification['transition']  = 'recording2processingquery'
+        notifications.put(notification)
+    
+
+def process_transition(transition, params):
+    global eva_state, _username
+
+    if transition == 'idle2listening':
         eva_led.set(Listen())
-        # open mic here!
         eva_state = 'listening'
         mic.start()
-        logging.info('WF Eva listening - Mic started')
+
+        username = params['username']
 
         if username : 
             print(username)
             _username = username
-            logging.info(f'WF username updated {username}')
         else:
             _username = None
-        
-    elif event in ['face_not_listen', 'not_faces'] and eva_state == 'listening':
+
+    elif transition == 'listening2idle':
         eva_led.set(Neutral())
-        # close mic here
         eva_state = 'idle'
         mic.stop()
-        logging.info('WF Eva Idle - Mic stopped')
 
-        if event == 'not_faces':
-            _username = None
-
-    elif event == 'recording_face':
-        eva_led.set(Progress(progress))
-    
-
-def mic_event_handler(event, audio=None):
-    global eva_state, _username
-    logging.info(f'MIC Event {event}')
-    if event == 'start_recording' and eva_state == 'listening':
+    elif transition == 'listening2recording':
         eva_state = 'recording'
         eva_led.set(Recording())
         server.prepare()
         #disconnect camera
         wf.stop()
+    
+    elif transition == 'listening_without_cam2recording':
+        eva_state = 'recording'
+        eva_led.set(Recording())
+        server.prepare()
 
-        logging.info(f'MIC Eva Recording - Wf stopped')
-        
-    if event == 'stop_recording' and eva_state == 'recording':
+    elif transition == 'recording2processingquery':
         eva_state = 'processing_query'
         eva_led.set(Neutral())
-        audio_response, action = server.query(audio, _username)
+        mic.stop()
 
-        logging.info(f'MIC Eva Processing Query')
+        audio = params['audio']
+
+        audio_response, action, continue_conversation = server.query(audio, _username)
 
         if audio_response:
-            logging.info(f'MIC Eva Speaking')
             eva_state = 'speaking'
             eva_led.set(Breath())
             speaker.play(audio_response)
 
         if action: # Execute associated action
-            logging.info(f'MIC Eva Executing action {action}')
             # Switch con tipos de acciones
             if action[0] == 'record_face':
-                #eva_led.set(Recording_face())
                 wf.record_face(action[1])
+        
+        if continue_conversation:
+            eva_led.set(Listen())
+            eva_state = 'listening_without_cam'
+            mic.start()
 
-        eva_state = 'idle' # TEMPORAL
-        eva_led.set(Neutral())
-        wf.start()
+        else:   
+            eva_state = 'idle' 
+            eva_led.set(Neutral())
+            wf.start()
+    
+    elif transition == 'recording_face':
+        eva_led.set(Progress(params['progress']))
+    
 
-        logging.info(f'MIC Eva Idle - Wf started')
 
-
+        
+    
 
 eva_led = EvaLed()
 wf = Wakeface(wf_event_handler)
@@ -96,8 +139,14 @@ print('Start!')
 try:
     while True:
         time.sleep(0.1)
+        notification = notifications.get()
+        
+        transition = notification['transition']
+        params = notification['params']
+        process_transition(transition, params)
 except KeyboardInterrupt:
     pass
+    
 
 wf.stop()
 mic.stop()
